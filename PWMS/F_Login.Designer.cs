fbc44a86c2b40b1d486efbc5b5c69248168f66cb
46135621 <985579956@qq.com>
2023-07-10 22:27:16 +0800

    init

---
+++ PWMS/F_Login.Designer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace PWMS
+{
+    partial class F_Login
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(373, 149);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(73, 31);
+            this.buttonLogin.TabIndex = 3;
+            this.buttonLogin.Text = "登陆";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(124, 156);
+            this.textName.Margin = new System.Windows.Forms.Padding(2);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(138, 21);
+            this.textName.TabIndex = 1;
+            this.textName.Text = "用户名";
+            this.textName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textName_KeyPress);
+            // 
+            // textPass
+            // 
+            this.textPass.Location = new System.Drawing.Point(124, 244);
+            this.textPass.Margin = new System.Windows.Forms.Padding(2);
+            this.textPass.Name = "textPass";
+            this.textPass.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textPass.Size = new System.Drawing.Size(138, 21);
+            this.textPass.TabIndex = 2;
+            this.textPass.Text = "密码";
+            this.textPass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textPass_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F);
+            this.label1.Location = new System.Drawing.Point(275, 61);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "登陆界面";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(373, 238);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "关闭";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonClose);
+            // 
+            // F_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textPass);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.buttonLogin);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "F_Login";
+            this.RightToLeftLayout = true;
+            this.Tag = 1;
+            this.Text = "Login";
+            this.Activated += new System.EventHandler(this.F_Login_Activated);
+            this.Load += new System.EventHandler(this.F_Login_load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonLogin;
+        private TextBox textName;
+        private TextBox textPass;
+        private Label label1;
+        private Button button1;
+    }
+}
+
